47f2bb86156697d39df1b60159585ad283787bb6
SUNANDA SRI KARUMURI <Karumuri2003@gmail.com>
2026-09-19 19:06:41 +0530

    Update backend.tf

--- EKS-Terraform/backend.tf
+++ EKS-Terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "devncloudtechdevops"
+    bucket       = "sridevncloudtechdevops"
     key          = "terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true
